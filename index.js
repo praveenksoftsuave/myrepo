ab3174411fc9c44eec906f20effe4ab3cdb31444
praveenksoftsuave <86826924+praveenksoftsuave@users.noreply.github.com>
2021-07-09 19:55:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ const app = require('express')();
 const bodyParser = require('body-parser');
 //my changes dfdff
 //fdsffdff dfggdgdfg
-//fggdfgdgdg
-//fdfdsfsfsf gdfggddfgf
+
 app.use(bodyParser.json())
 
 app.get('/', (req, res)=> {
